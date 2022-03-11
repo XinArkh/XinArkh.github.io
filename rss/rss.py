98aea32156edce1560edf81c230c7d115b63e260
Wu Xin <wuhsin@zju.edu.cn>
2022-03-11 23:02:04 +0800

    add rss scripts

--- rss/rss.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-from news_me_tzgg import me_tzgg
-
-
-me_tzgg()
---
+++ rss/zju-me-rss/url2article.py
@@ -0,0 +1,23 @@
+import requests
+import rss_engine
+
+
+api_url = 'http://url2api.applinzi.com/article'
+my_token = 'BnYZtNTSRK-2mZ6zTtpgxg'
+
+
+def get_article(url):
+    html = rss_engine.get_url_content(url)
+    query_string = {'token': my_token, 'url': url,}
+    headers = { 'content-type': "text/html", }
+    article = requests.post(api_url, params=query_string, headers=headers, data=html.encode('utf-8'))
+    if article.status_code != 200:
+        raise ValueError('Request response %d, expecting 200!' % article.status_code)
+
+    return article.json()
+
+
+if __name__ == '__main__':
+    article = get_article('http://me.zju.edu.cn/meoffice/2022/0217/c6245a2498234/page.htm')
+    print(article.keys())
+    print(article)
